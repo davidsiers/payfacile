a5375926b67c350f451e9780ebe2e7fb8f573c4e
majka-meteor <maria.nemcova@outlook.com>
2015-05-03 22:54:04 +0200

    fff

--- server/method.js
+++ server/method.js
@@ -1,19 +1,20 @@
 ServiceConfiguration.configurations.upsert(
-
     { service: "google" },
-
     {
-
         $set: {
-
             clientId: "170762067850-9nkab4bqrpnrnr1g2gcptsfe830lv8sg.apps.googleusercontent.com",
-
             loginStyle: "popup",
-
             secret: "nOF0_YYurbsGqa7CQ8hKFema"
-
         }
-
     }
-
 );
+
+ServiceConfiguration.configurations.upsert(
+{ service: "facebook" },
+{
+    $set: {
+            appId: '812677155488375',
+            loginStyle: "popup",
+            secret: 'd0d6a49f217cf5db3b9b4e950918e6c8'
+    }
+});
