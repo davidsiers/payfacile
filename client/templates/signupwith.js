e8d02b8b87f8f8561b33f78cb78c00d1aee054b1
majka-meteor <maria.nemcova@outlook.com>
2015-05-06 14:06:59 +0200

    fff

---
+++ client/templates/signupwith.js
@@ -0,0 +1,27 @@
+Template.signupwith.events({
+    'click .btn-google': function(e) {
+        e.preventDefault();
+        Meteor.loginWithGoogle({
+                requestPermissions: ['email']}
+            , function(result,error){
+                if(error)
+                    console.log(error.reason);
+            }
+        );
+    },
+    'click #facebook-login': function(e) {
+        e.preventDefault();
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
+    }
+});
