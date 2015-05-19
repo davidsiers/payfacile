eb68873f19c53168753b996d67507c777e242de1
majka-meteor <maria.nemcova@outlook.com>
2015-05-19 21:27:00 +0200

    New version material design

--- client/templates/signup.js
+++ client/templates/signup.js
@@ -40,3 +40,32 @@ Template.signup.events({
         $("#myForm").toggle();
     });
 });*/
+
+Template.signupwith.events({
+    'click .btn-google': function(e) {
+        e.preventDefault();
+        Meteor.loginWithGoogle({
+                requestPermissions: ['email']}
+            , function(result,error){
+                if(error)
+                    console.log(error.reason);
+            }
+        );
+    },
+    'click #facebook-login': function(e) {
+        e.preventDefault();
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
+    }
+});
+
