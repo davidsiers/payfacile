be7a75924e171443d85b5ecf39f97aa6f12a2113
majka-meteor <maria.nemcova@outlook.com>
2015-05-24 21:45:31 +0200

    finalna verzia2

--- client/templates/header.js
+++ client/templates/header.js
@@ -2,10 +2,10 @@
 Template.header.helpers({
         'email': function(){
             if(Meteor.user() && Meteor.user().emails){
-                return Meteor.user().emails[0].address;
+                return Meteor.user().emails[0].address; //default user mail address
             }
             else{
-                return '';
+                return ''; //to go to else condition in Blaze (other accounts like Google or FB)
             }
         }
 });
